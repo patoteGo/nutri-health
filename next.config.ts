87c68cc00d33008a63007ee572d91d11e677a0b0
Patricio Gonzalez <patote.gonzalez@gmail.com>
2025-04-19 07:14:02 -0300

    pwa fixes

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,21 @@
 import type { NextConfig } from "next";
+const withPWA = require("next-pwa");
+
+const pwaConfig = {
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+};
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   images: {
     domains: [
-      'lh3.googleusercontent.com', // Google avatars
-      'avatars.githubusercontent.com', // GitHub avatars
+      "lh3.googleusercontent.com", // Google avatars
+      "avatars.githubusercontent.com", // GitHub avatars
     ],
   },
 };
 
-export default nextConfig;
+export default withPWA(pwaConfig)(nextConfig);
