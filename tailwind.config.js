69cd00b62ea47f836120aa5febba688ebfb7c723
Patricio Gonzalez <patote.gonzalez@gmail.com>
2025-04-19 22:21:27 -0300

    update colors

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          light: '#43C59E', // Green as primary
+          DEFAULT: '#43C59E',
+          dark: '#43C59E',
+        },
+        secondary: {
+          light: '#3D7068', // Deep teal as secondary
+          dark: '#14453D',
+        },
+        accent: {
+          light: '#14453D', // Dark accent
+          dark: '#3D7068',
+        },
+        bg: {
+          light: '#FFFFFF',
+          dark: '#14453D',
+        },
+        text: {
+          light: '#14453D',
+          dark: '#43C59E', // Updated to match dark mode foreground
+        },
+      },
+    },
+  },
+  plugins: [],
+};
